5bbdd7f1e8519f1101d51c1e25435ea320b7524e
Carlos Daniel Vega Ramirez <daniel.vega_99@hotmail.com>
2024-05-21 23:56:52 -0500

    C8

--- scripts/lanzamiento.js
+++ scripts/lanzamiento.js
@@ -1,11 +1,9 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-
-function changeSlide(direction) {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + direction + slides.length) % slides.length;
-    slides[currentSlide].classList.add('active');
-}
-
-// Inicializar el primer slide como activo
-slides[currentSlide].classList.add('active');
+function moveCarousel(direction) {
+    const container = document.querySelector('.card-carousel');
+    const scrollAmount = direction === 1 ? container.scrollWidth / 3 : -container.scrollWidth / 3;
+    container.scrollBy({
+      left: scrollAmount,
+      behavior: 'smooth'
+    });
+  }
+  
